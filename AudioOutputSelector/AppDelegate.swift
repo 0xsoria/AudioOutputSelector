1df9b914ce2178f07acd55810a660776f2e66325
Gabriel Sória <gabrielss.rock@me.com>
2021-02-13 16:53:21 -0300

    Initial Commit

---
+++ AudioOutputSelector/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AudioOutputSelector
+//
+//  Created by Gabriel Soria Souza on 13/02/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AudioOutputSelector/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AudioOutputSelector
+//
+//  Created by Gabriel Soria Souza on 13/02/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
